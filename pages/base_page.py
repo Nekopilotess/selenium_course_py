ef5660f9fc2a64a843009b23a4501242fff89423
[neko] <a.kazakova@berlin.ru>
2022-12-23 10:41:57 +0300

    4.2 step 9 and 10

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,5 +1,5 @@
-from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException
+
 
 class BasePage():
     def __init__(self, browser, url, timeout=10):
--- test_main_page.py
+++ test_main_page.py
@@ -1,19 +1,20 @@
-from .pages.main_page import MainPage
 from selenium.webdriver.common.by import By
+
+from .pages.login_page import LoginPage
+from .pages.main_page import MainPage
 
 
 def go_to_login_page(browser):
     login_link = browser.find_element(By.CSS_SELECTOR, "#login_link")
     login_link.click()
+    alert = self.browser.switch_to.alert
+    alert.accept()
+
 
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
-    page = MainPage(browser, link) # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
-    page.open()             # открываем страницу
+    page = MainPage(browser, link)  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
+    page.open()  # открываем страницу
     page.go_to_login_page() # выполняем метод страницы — переходим на страницу логина
-
-def test_guest_should_see_login_link(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
-    page = MainPage(browser, link)
-    page.open()
-    page.should_be_login_link()
+    login_page = LoginPage(browser, browser.current_url)   #сохраняем в переменную страницу
+    login_page.should_be_login_page()  # используем методы новой страницы в тесте
